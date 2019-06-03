13035959e43a312a34e3c93e4924911106974987
liyunjiao2048@163.com <liyunjiao@baidu.com>
2019-06-03 17:00:14 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+/*
+* @Author: liyunjiao2048@163.com
+* @Date:   2019-06-03 12:42:56
+* @Last Modified by:   liyunjiao2048@163.com
+* @Last Modified time: 2019-06-03 12:47:28
+*/
+
+module.exports = {
+    configureWebpack:{
+        
+    }
+}
